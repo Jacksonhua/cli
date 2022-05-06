ad3dc29c0f79180ea0649e1dcfedffb1ba8544db
wang.hh.2@pg.com <wang.hh.2@pg.com>
2022-05-06 16:02:59 +0800

    first commit

---
+++ src/main/java/com/whh/demo/test/Test.java
@@ -0,0 +1,18 @@
+package com.whh.demo.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @description:
+ * @author: Artermus wang on 2022-05-02 17:33
+ */
+@Slf4j
+public class Test {
+    Logger logger = LoggerFactory.getLogger(Test.class);
+    public static void main(String[] args) {
+        log.info("12345ty");
+    }
+
+}
